ed61718449b25053a481b3e077bdd19bbb4eb5d5
brianhouse <brian.house@gmail.com>
2013-09-03 21:58:36 -0400

    added parsing for ambit

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import geojson, csv, dateutil, datetime, model, time, os, zipfile, pytz
+import geojson, csv, dateutil, datetime, model, time, os, zipfile, pytz, xmltodict, json
+import xml.etree.ElementTree as ET        
 from housepy import config, log, util, strings, emailer
 
 
@@ -12,19 +13,19 @@ def injest_geo_feature(filename, kind):
         for r, row in enumerate(rows):
             if r == 0:
                 for i, item in enumerate(row):
-                    headings[item.lower()] = i
+                    headings[item] = i
                 continue
             try:
-                dt = util.parse_date("%s %s" % (row[headings['date']], row[headings['time']]), tz=config['local_tz'], dayfirst=True)
+                dt = util.parse_date("%s %s" % (row[headings['Date']], row[headings['Time']]), tz=config['local_tz'], dayfirst=True)
                 t = util.timestamp(dt)
                 try:
-                    coordinates = strings.as_numeric(row[headings['latitude']]), strings.as_numeric(row[headings['longitude']]), strings.as_numeric(row[headings['altitude']])
+                    coordinates = strings.as_numeric(row[headings['Latitude']]), strings.as_numeric(row[headings['Longitude']]), strings.as_numeric(row[headings['Altitude']])
                 except Exception as e:
                     log.error("Missing coordinates! Skipping...")
                     continue
-                properties = {'datetime': dt.strftime("%Y-%m-%dT%H:%M:%S%z"), 't_utc': t, 'contenttype': kind}
+                properties = {'DateTime': dt.strftime("%Y-%m-%dT%H:%M:%S%z"), 't_utc': t, 'ContentType': kind}
                 for heading in headings:
-                    if heading not in ['date', 'time', 'latitude', 'longitude', 'altitude']:
+                    if heading not in ['Date', 'Time', 'Latitude', 'Longitude', 'Altitude']:
                         try:
                             properties[heading] = strings.as_numeric(row[headings[heading]])
                         except IndexError:
@@ -37,9 +38,37 @@ def injest_geo_feature(filename, kind):
                 continue
 
 
-def injest_ambit():    
-    pass
-    
+def injest_ambit(filename):    
+    with open(filename, 'r') as f:
+        content = f.read()        
+        content = content.split("<IBI>")[0]
+        parts = content.split("</header>")
+        header = parts[0] + "</header>"
+        header = xmltodict.parse(header.encode('utf-8'))
+        person = header['header']['Activity'].replace("OWBS ", "") 
+        content = parts[-1].encode('utf-8')
+        samples = xmltodict.parse(content)['Samples']['Sample']
+        for s, sample in enumerate(samples):
+            if 'VerticalSpeed' not in sample:
+                continue
+            for key, value in sample.items():
+                if key == "UTC":
+                    dt = util.parse_date(value) # these are UTC in the data
+                    t = util.timestamp(dt)
+                    del sample[key]
+                    continue
+                if type(value) != str:
+                    del sample[key]
+                    continue
+                sample[key] = strings.as_numeric(value)
+            sample['DateTime'] = dt.astimezone(pytz.timezone(config['local_tz'])).strftime("%Y-%m-%dT%H:%M:%S%z")
+            sample['t_utc'] = t
+            sample['ContentType'] = 'ambit'
+            sample['Person'] = person
+            feature = geojson.Feature(properties=sample)
+            model.insert_feature('ambit', t, geojson.dumps(feature))
+            log.info("Inserted feature")
+
 
 def injest_media(filename, kind):
     dt = datetime.datetime.strptime(filename.split('_')[0], "%d%m%Y%H%M")
@@ -97,12 +126,12 @@ for message in messages:
                 p = path.split('.')[0]
                 os.mkdir(p)
                 with ZipFile(path, 'r') as archive:
-                archive.extractall(p)
-                for filename in os.listdir(p):
-                    if kind == 'ambit':
-                        injest_ambit(os.path.join(p, filename))
-                    elif kind == 'image' or kind == 'audio':
-                        injest_media(os.path.join(p, filename), kind)
+                    archive.extractall(p)
+                    for filename in os.listdir(p):
+                        if kind == 'ambit':
+                            injest_ambit(os.path.join(p, filename))
+                        elif kind == 'image' or kind == 'audio':
+                            injest_media(os.path.join(p, filename), kind)
 
             elif kind == 'position':
                 pass
