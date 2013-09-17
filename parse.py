68c25dd9eec45a962ec2b556416625112601beed
brianhouse <brian.house@gmail.com>
2013-09-17 00:56:38 -0400

    tweak to logging

--- parse.py
+++ parse.py
@@ -210,7 +210,8 @@ def ingest_beacon(content):
 
 def main():    
     messages = emailer.fetch()
-    log.info("Fetched %s new messages..." % len(messages))
+    if len(messages) > 0:
+        log.info("Fetched %s new messages..." % len(messages))
     for m, message in enumerate(messages):
         if message['from'] not in config['incoming']:
             log.warning("Received bunk email from %s" % message['from'])
