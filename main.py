2c2d48099b9984ec1e888f51bbc9d15b8541e996
Jer Thorp <blprnt@blprnt.com>
2013-09-20 01:26:48 -0400

    Trying to make data load day by day

--- main.py
+++ main.py
@@ -10,14 +10,14 @@ class Home(server.Handler):
     def get(self, page=None):
         if len(page):
             return self.not_found()
-        return self.render("map2.html")
+        return self.render("map3.html")
 
 class Beta(server.Handler):
 
     def get(self, page=None):
         if len(page):
             return self.not_found()
-        return self.render("map3.html")
+        return self.render("map4.html")
 
 class Images(server.Handler):
 
