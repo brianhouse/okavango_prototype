2b7d22a1176142a6f362d9d2028cdf12727214c5
Jer Thorp <blprnt@blprnt.com>
2014-08-06 11:47:49 +1200

    Working on file upload.

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ class Userform(tornado.web.RequestHandler):
 class Upload(tornado.web.RequestHandler):
     def post(self):
         fileinfo = self.request.files['filearg'][0]
-        print "fileinfo is", fileinfo
+        print ("fileinfo is" +  fileinfo)
         fname = fileinfo['filename']
         extn = os.path.splitext(fname)[1]
         cname = str(uuid.uuid4()) + extn
