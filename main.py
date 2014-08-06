06b3c83a94722d174b29f66c42d69f705cedb71b
Jer Thorp <blprnt@blprnt.com>
2014-08-06 12:05:51 +1200

    Working on file upload.

--- main.py
+++ main.py
@@ -54,7 +54,6 @@ class Upload(server.Handler):
         extn = os.path.splitext(fname)[1]
         cname = str(uuid.uuid4()) + extn
         fh = open(__UPLOADS__ + cname, 'w')
-        fh.write(fileinfo['body'])
         self.finish(cname + " is uploaded!! Check %s folder" %__UPLOADS__)
 
 class Api(server.Handler):
