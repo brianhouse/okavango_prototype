604a68552288c9b7af9642ca4894a5ea93bfcc2b
Jer Thorp <blprnt@blprnt.com>
2014-08-06 13:48:45 +1200

    Getting upload to server working.

--- main.py
+++ main.py
@@ -56,8 +56,7 @@ class Upload(server.Handler):
         cname = fname #str(uuid.uuid4()) + extn
         #body = self.request.data
         fh = open(__UPLOADS__ + cname, 'w')
-        print(fileinfo);
-        fh.write(fileinfo)
+        fh.write(fileinfo['body'])
         self.finish(cname + " is uploaded!! Check %s folder" %__UPLOADS__)
 
 class Api(server.Handler):
