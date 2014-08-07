b874809704d01f1f30c01fe56c54f884c3164362
Jer Thorp <blprnt@blprnt.com>
2014-08-08 07:06:01 +1200

    Trying to match media w/ coordinates.

--- model.py
+++ model.py
@@ -54,7 +54,7 @@ def get_protect(kind):
 def get_coords_by_time(time):
     db.execute("SELECT t,data FROM features WHERE kind='beacon' AND t < ? ORDER BY t DESC LIMIT 1", (time,))
     result = db.fetchone()
-    closeFeature = row['data'];
+    closeFeature = result['data'];
 
     print("CLOSEST FEATURE TO ? IS ?", (time,closeFeature))
     return closeFeature;
