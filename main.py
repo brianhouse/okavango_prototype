8dbcb5b395c3f1c990f74019deff836fa19df386
Jer Thorp <blprnt@blprnt.com>
2014-08-08 16:36:02 +1200

    Audio upload.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import datetime, pytz, geojson, model, os, uuid, shutil
+import datetime, pytz, geojson, model, os, uuid, shutil, subprocess
 from housepy import config, log, server, util, process
 
 process.secure_pid(os.path.abspath(os.path.join(os.path.dirname(__file__), "run")))
