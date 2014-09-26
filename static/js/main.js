14153a266f01629cb7d2ae98bece39843f7423a1
iaaaan <ian.ardouin@gmail.com>
2014-09-26 20:03:16 +0200

    fix firefox

--- static/js/main.js
+++ static/js/main.js
@@ -71,7 +71,7 @@ var setVideoHeight = function(){
 			// var w = d3.select('#video svg.playButton').node().clientWidth);
 			// return
 			// console.log('test: ' + d3.select('#video svg.playButton').node().clientWidth);
-			return d3.select('#video svg.playButton').node().clientWidth
+			return '90'
 		})
 			
 	d3.select('#video')
