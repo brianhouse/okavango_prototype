3662bd3555d3ea8fc09989ce1ae9bb0808afdd3b
iaaaan <ian.ardouin@gmail.com>
2014-09-26 16:35:41 +0200

    clickable tweet

--- static/js/main.js
+++ static/js/main.js
@@ -1027,15 +1027,17 @@ var focusTweet = function(marker){
 
     if(tweetFound){
     	console.log('focusing tweet: ' + marker.feature.properties.tweet.text);
-    	clearTimeout(closeFeedTimer);
     	if(currentPage != 'Twitter') toggleTwitterPanel();
     	if(startTime - (tweetsQueue[tweetCounter].time/1000 - 300) > 20) closeFeedTimer = setTimeout(toggleTwitterPanel,10000);
 	    requestAnimationFrame(function(){
-		    d3.select('#twitterWrapper')
-		    	.transition()
-		    	.duration(400)
-		    	.ease("cubic-in-out")
-		    	.style('margin-top',(20-h)+'px');
+		    requestAnimationFrame(function(){
+		    	console.log(d3.select('#twitterWrapper'));
+			    d3.select('#twitterWrapper')
+			    	.transition()
+			    	.duration(400)
+			    	.ease("cubic-in-out")
+			    	.style('margin-top',(20-h)+'px');
+		    });
 	    });
     }
 
